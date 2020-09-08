749c7a7adb728e31932e69423a73ce93a1f1fa62
Suno <dev.suno01@gmail.com>
2020-09-08 20:16:16 +0800

    删除无用代码

--- src/components/Record/Calendar/Calendar.vue
+++ src/components/Record/Calendar/Calendar.vue
@@ -8,20 +8,6 @@
         <div class="week">
             <span :key="index" class="dayOfWeek" v-for="(day,index) in week">{{"周"+day}}</span>
         </div>
-<!--        <Swiperwrap/>
-       <div class="month">
-                <span :class="[
-                        {'month-class': isCurrentMonth(getDateHtml(day.year, day.month, day.day) )},
-                        {'todayBg': isCurrentDay(getDateHtml(day.year, day.month, day.day))},
-                        {selected:day.year+'-'+ (day.month+1) +'-'+day.day===selectedDate}
-                    ]"
-                      :key="index"
-                      @click="handleClickDay(day)"
-                      v-for="(day,index) in calendarArr">
-                    {{formatDate(day.day)}}
-                </span>
-        </div>-->
-
     </div>
 </template>
 
@@ -53,10 +39,6 @@
 
         //日历数组
         calendarArr: Array<object> = [];
-        
-        //选中日期
-/*        selectedDate = this.time.year + '-' + (this.time.month + 1) + '-' + this.time.day;*/
-
 
         mounted() {
             if (this.weekDay === 5 || this.weekDay === 6 || this.weekDay === 7) {
@@ -72,61 +54,6 @@
                 });
             }
         }
-/*
-        // 是否是当前月
-        isCurrentMonth(date: Date) {
-            const { year: currentYear, month: currentMonth } =
-                helper.getNewDate(helper.getDate(this.time.year, this.time.month, 1));
-            const { year, month } = helper.getNewDate(date);
-            return currentYear === year && currentMonth === month;
-        }
-
-        // 是否是今天
-        isCurrentDay(date: Date) {
-            const { year: currentYear, month: currentMonth, day: currentDay } =
-                helper.getNewDate(helper.getDate(this.time.year, this.time.month, this.time.day));
-            const { year, month, day } = helper.getNewDate(date);
-            return currentYear === year && currentMonth === month && currentDay === day;
-        }
-
-        // 上一个月
-        prevMonth() {
-            const prevMonth = helper.getDate(this.time.year, this.time.month, 1);
-            prevMonth.setMonth(prevMonth.getMonth() - 1);
-            this.time = helper.getNewDate(prevMonth);
-            this.$emit('prevMonth');
-        }
-
-        // 下一个月
-        nextMonth() {
-            const nextMonth = helper.getDate(this.time.year, this.time.month, 1);
-            nextMonth.setMonth(nextMonth.getMonth() + 1);
-            this.time = helper.getNewDate(nextMonth);
-            this.$emit('nextMonth');
-        }
-
-        handleClickDay(day: { year: number; month: number; day: number }) {
-            // this.$forceUpdate();
-            // this.$emit('handleClickDay', day);
-            // this.$set(day, 'clickDay', true);
-            this.selectedDate = day.year + '-' + (day.month + 1) + '-' + day.day;
-        }
-        getDateHtml = (year: number, month: number, day: number) => {
-            return new Date(year, month, day);
-        };
-
-        //监听手指开始滑动事件
-                touchStart(event) {
-                    this.$emit('touchstart', event)
-
-                    this.touchStartX = event.touches[0].clientX
-                    this.touchStartY = event.touches[0].clientY
-                    this.touch = {
-                        x: 0,
-                        y: 0
-                    }
-
-                }*/
         currentMonthMaxDay = () => {
             const date = new Date();
             return new Date(date.getFullYear(), date.getMonth(), 0).getDate();
