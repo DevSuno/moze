08bc8f1ab0e5e88134b088668d7b2ca259896286
Suno <dev.suno01@gmail.com>
2020-09-21 11:45:21 +0800

    Once 页面 Tabs 添加动态 class

--- src/components/Once/Tabs.vue
+++ src/components/Once/Tabs.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
         <ul class="tabs">
-            <li :class="[{selected:true}] ">支出</li>
-            <li>收入</li>
+            <li :class="[{selected: tab.tab === selectedTab}] " :key="tab"
+                @click="changeSelectedTab(tab.tab)"
+                v-for="tab in tabs"
+            >{{tab.tab}}
+            </li>
+
         </ul>
     </div>
 </template>
@@ -11,33 +15,43 @@
     import Vue from 'vue';
     import { Component } from 'vue-property-decorator';
 
-    @Component
-    export default class Tabs extends Vue {
+    type Tab = [{ tab: string }, { tab: string }]
 
+    @Component
+
+    export default class Tabs extends Vue {
+        selectedTab = '支出';
+        tabs: Tab = [{ tab: '支出' }, { tab: '收入' }];
+        changeSelectedTab(tab: string){
+            this.selectedTab = tab
+        }
     }
 </script>
 
 <style lang="scss" scoped>
     @import "src/assets/style/helper.scss";
-.tabs {
-    margin-top: 20px;
-    display: flex;
-    flex-direction: row;
-    > li {
-        width: 50%;
-        text-align: center;
-        padding: 10px 0;
-        position: relative;
-        &.selected::after {
-            content: '';
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            height: 2px;
-            background: $color-blueselected;
-        }
 
+    .tabs {
+        margin-top: 20px;
+        display: flex;
+        flex-direction: row;
+
+        > li {
+            width: 50%;
+            text-align: center;
+            padding: 10px 0;
+            position: relative;
+
+            &.selected::after {
+                content: '';
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                width: 100%;
+                height: 2px;
+                background: $color-blueselected;
+            }
+
+        }
     }
-}
 </style>
