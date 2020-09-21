c1165ce939190630f8f02bf288e48d5ae0aa8931
Suno <dev.suno01@gmail.com>
2020-09-21 15:31:46 +0800

    修改 Once 页面 Tabs 的样式

--- src/components/Once/Tabs.vue
+++ src/components/Once/Tabs.vue
@@ -25,13 +25,14 @@
         tabs: Tab = [{ tab: '支出' }, { tab: '收入' }];
 
         //初始
-        mounted(){
-            this.eventBus.$emit('updateSelectedTab',this.selectedTab)
+        mounted() {
+            this.eventBus.$emit('updateSelectedTab', this.selectedTab);
         }
+
         //实时更新
         changeSelectedTab(tab: string) {
             this.selectedTab = tab;
-            this.eventBus.$emit('updateSelectedTab',this.selectedTab)
+            this.eventBus.$emit('updateSelectedTab', this.selectedTab);
         }
     }
 </script>
@@ -46,9 +47,14 @@
 
         > li {
             width: 50%;
+            color: $color-normal;
             text-align: center;
             padding: 15px 0;
             position: relative;
+
+            &.selected {
+                color: $color;
+            }
 
             &.selected::after {
                 content: '';
