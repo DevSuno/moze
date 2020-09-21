9e9a561c2be4194abdab83daad8b3fb9c0b646d8
Suno <dev.suno01@gmail.com>
2020-09-21 11:12:25 +0800

    重新封装 Icon

--- src/components/Once/Ico.vue
+++ src/components/Once/Ico.vue
@@ -1,42 +1,25 @@
 <template>
     <div>
-        <div class="pay">
-            <Icon name="eat"></Icon>
-            <span>饮食</span>
-            <Icon name="traffic"></Icon>
-            <span>交通</span>
-            <Icon name="recreation"></Icon>
-            <span>娱乐</span>
-            <Icon name="hospital"></Icon>
-            <span>医院</span>
-            <Icon name="family"></Icon>
-            <span>家庭</span>
-            <Icon name="shop"></Icon>
-            <span>购物</span>
+        <div :key="pay" class="pay" v-for="pay in payArr">
+            <Icon :name="pay.name"></Icon>
+            <span>{{pay.text}}</span>
         </div>
-        <div class="earning">
-            <Icon name="wage"></Icon>
-            <span>工资</span>
-
-            <Icon name="bonus"></Icon>
-            <span>奖金</span>
-
-            <Icon name="repayment"></Icon>
-            <span>还款</span>
-
-            <Icon name="interest"></Icon>
-            <span>利息</span>
-
+        --
+        <div :key="earning" class="earning" v-for="earning in earningArr">
+            <Icon :name="earning.name"></Icon>
+            <span>{{earning.text}}</span>
         </div>
     </div>
 </template>
 
 <script lang="ts">
     import Vue from 'vue';
-    import { Component } from 'vue-property-decorator';
+    import { Component, Prop } from 'vue-property-decorator';
 
     @Component
     export default class Ico extends Vue {
+        @Prop() payArr!: [{}];
+        @Prop() earningArr!: [{}];
 
     }
 </script>
--- src/views/Once.vue
+++ src/views/Once.vue
@@ -2,7 +2,7 @@
     <Layout class-prefix="layout">
         <Topbar></Topbar>
         <Tabs></Tabs>
-        <Ico/>
+        <Ico :payArr="payArr" :earningArr="earningArr"></Ico>
 
         <div>金额</div>
         <div>名称</div>
@@ -22,7 +22,23 @@
     @Component({
         components: { Ico, Tabs, Topbar, }
     })
+
     export default class Once extends Vue {
+        payArr: {} =[
+            {name:'eat',text:'饮食'},
+            {name:'traffic',text:'交通'},
+            {name:'recreation',text:'娱乐'},
+            {name:'hospital',text:'医院'},
+            {name:'family',text:'家庭'},
+            {name:'shop',text:'购物'}
+        ]
+        earningArr: {} = [
+            {name:'wage',text:'工资'},
+            {name:'bonus',text:'奖金'},
+            {name:'repayment',text:'还款'},
+            {name:'interest',text:'利息'},
+
+        ]
 
     }
 </script>
