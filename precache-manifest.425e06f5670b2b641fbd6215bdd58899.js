899a744afa55635509b833450b68771413b0cd50
Suno <dev.suno01@gmail.com>
2020-10-23 16:00:55 +0800

    deploy

---
+++ precache-manifest.425e06f5670b2b641fbd6215bdd58899.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51bc58ad9cb99bfbe054",
+    "url": "css/app.85748289.css"
+  },
+  {
+    "revision": "94e03ba2c1b608ed9d00",
+    "url": "css/chunk-vendors.c2c417c0.css"
+  },
+  {
+    "revision": "c82751579f3481642e618dbf95fd91cf",
+    "url": "index.html"
+  },
+  {
+    "revision": "51bc58ad9cb99bfbe054",
+    "url": "js/app.46f9399d.js"
+  },
+  {
+    "revision": "94e03ba2c1b608ed9d00",
+    "url": "js/chunk-vendors.23165292.js"
+  },
+  {
+    "revision": "fc17eb22641c4a00df04e5e15d6988b3",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "c533c26c9ecdad3aabcd124d5f8c4a82",
+    "url": "qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
